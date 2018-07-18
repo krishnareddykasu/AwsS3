c7cdcf48a65d26b302968cdaa80164d703737422
KrishnaReddyk <krishnareddyk@dbs.com>
2018-07-18 12:05:40 +0530

    accessKey Changed

--- src/main/java/com/krishna/s3/Credentials.java
+++ src/main/java/com/krishna/s3/Credentials.java
@@ -1,11 +1,11 @@
 package com.krishna.s3;
 /**
- * Created by KrishnaReddy Kasu.
+ * Created by KrishnaReddy Kasu. The Access Key_ID and ACCESS kEY Mentioned here are my Person AWS accoount
  */
 public class Credentials {
 
 
-    public static final String access_key_id= "AKIAIWZ6HVASGTFAYJ7Q";
+    public static final String access_key_id= "AKIA-IWZ6HV-ASGTF-AYJ7Q";
 
     public static final String secret_access_key ="KRUjYMe8X4zb2qXYlWoVuRmnrsW18akXq+zPTSsb";
     public static final String endPointUrl="https://s3.console.aws.amazon.com/s3/buckets";
